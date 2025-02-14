a4111dbd052baea7fa8d7b077df7879b0063d02f
Kadir-Saliu <kadir_saliu@hotmail.de>
2025-02-14 17:49:06 +0100

    git commit -m "update"

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- let myDishes = [
+let myDishes = [
   {
     name: "Pizza Krabben",
     price: 9.50,
@@ -19,13 +19,18 @@
     price: 13.00,
     description: "mit Pizzabrötchen ",
   },
+  {
+    name: "Pizza Bolognese",
+    price: 9.50,
+    description: "mit Tomatensoße, Gouda, Hackfleich und Zwiebel ",
+  },
 ];
 
 let dessert = [];
 
 let basketCarts = [];
 
-let finalSum = [];  
+let finalSum = [];
 
 function renderInformation() {
   let contentRef = document.getElementById("content");
@@ -39,8 +44,12 @@ function getInformationTemplate(indexNote) {
   return `<div class="menu" >
         <div>
             <h3> ${myDishes[indexNote].name}</h3>
-            <p> <strong> Preis: </strong> ${myDishes[indexNote].price.toFixed(2)} €</p>
-            <p> <strong> Beschreibung: </strong> ${myDishes[indexNote].description}</p>
+            <p> <strong> Preis: </strong> ${myDishes[indexNote].price.toFixed(
+              2
+            )} €</p>
+            <p> <strong> Beschreibung: </strong> ${
+              myDishes[indexNote].description
+            }</p>
         </div>
             <div>
             <button onclick="addToBasket(${indexNote})" >+</button>
@@ -63,9 +72,13 @@ function getBasketTemplate(indexBasket) {
   <div  class="basket-menu" >
           <button onclick="minusAmmountPrice(${indexBasket})">-</button>
           <div>
-              <p id="ammountPriceUp${indexBasket}"> ${basketCarts[indexBasket].amount} x</p>
+              <p id="ammountPriceUp${indexBasket}"> ${
+    basketCarts[indexBasket].amount
+  } x</p>
               <h3> ${basketCarts[indexBasket].name}</h3>
-              <p id='changePrice${indexBasket}'> ${basketCarts[indexBasket].price.toFixed(2) * basketCarts[indexBasket].amount} €</p>
+              <p id='changePrice${indexBasket}'> ${
+    basketCarts[indexBasket].price.toFixed(2) * basketCarts[indexBasket].amount
+  } €</p>
               <p> ${basketCarts[indexBasket].description}</p>
           </div>
             <button onclick="plusAmmountPrice(${indexBasket})" >+</button>
@@ -73,31 +86,26 @@ function getBasketTemplate(indexBasket) {
   </div>`;
 }
 
-
-
 function addToBasket(indexNote) {
-  let dishName = myDishes[indexNote]['name']
+  let dishName = myDishes[indexNote]["name"];
   const dishBasketIndex = basketCarts.findIndex((dish) => {
-      return dish.name === dishName;
+    return dish.name === dishName;
   });
 
-  if(dishBasketIndex !== -1) {
-    basketCarts[dishBasketIndex]['amount']++
+  if (dishBasketIndex !== -1) {
+    basketCarts[dishBasketIndex]["amount"]++;
   } else {
     let newDish = {
-      name: myDishes[indexNote]['name'],
-      description: myDishes[indexNote]['description'],
-      price: myDishes[indexNote]['price'],
-      amount: 1
-    }
-    basketCarts.push(newDish)
+      name: myDishes[indexNote]["name"],
+      description: myDishes[indexNote]["description"],
+      price: myDishes[indexNote]["price"],
+      amount: 1,
+    };
+    basketCarts.push(newDish);
   }
   renderBasket();
-  cost()
+  cost();
 }
-
-
-
 
 function plusAmmountPrice(indexBasket) {
   let contetRef = document.getElementById(`ammountPriceUp${indexBasket}`);
@@ -108,8 +116,7 @@ function plusAmmountPrice(indexBasket) {
   currentPrice.innerHTML = "";
   let sum = basketCarts[indexBasket].amount * basketCarts[indexBasket].price;
   currentPrice.innerHTML = sum + " €";
-  cost()
-  
+  cost();
 }
 
 function minusAmmountPrice(indexBasket) {
@@ -117,48 +124,55 @@ function minusAmmountPrice(indexBasket) {
   basketCarts[indexBasket].amount = basketCarts[indexBasket].amount - 1;
   contetRef.innerHTML = `  <p>${basketCarts[indexBasket].amount} x </p>`;
 
-
-
   let currentPrice = document.getElementById(`changePrice${indexBasket}`);
   currentPrice.innerHTML = "";
   let sum = basketCarts[indexBasket].amount * basketCarts[indexBasket].price;
   currentPrice.innerHTML = sum + "€";
 
   if (basketCarts[indexBasket].amount < 1) {
-    let basketCart = basketCarts[indexBasket];
-    basketCarts.splice(basketCart,1);
+    basketCarts.splice(indexBasket, 1);
     console.log(basketCarts);
     renderBasket();
   }
 
-  
- cost() 
- 
+  cost();
 }
 
-function cost(){
-  let currentCost = document.getElementById('subtotal')
-  console.log('basket: ', basketCarts);
+function cost() {
+  let currentCost = document.getElementById("subtotal");
+  let deliveryCosts = document.getElementById("delivery");
+  let total = document.getElementById("total-sum");
+  
+
+
+  console.log("basket: ", basketCarts);
   sum = 0;
-  for(let index = 0; index < basketCarts.length; index++){
-    sum += basketCarts[index].amount * basketCarts[index].price.toFixed(2)
+  for (let index = 0; index < basketCarts.length; index++) {
+    sum += basketCarts[index].amount * basketCarts[index].price.toFixed(2);
   }
-  currentCost.innerHTML = 'Zwischensumme: ' + sum + " €";
-  console.log('sum: ', sum);
+  currentCost.innerHTML = "Zwischensumme: " + sum.toFixed(2) + " €";
+  console.log("sum: ", sum);
+  let deliveryCost = sum > 0 ? 5 : 0
+  deliveryCosts.innerHTML = ` Lieferkosten: ${deliveryCost.toFixed(2)} €`;
 
-  let deliveryCosts = document.getElementById('delivery')
-  deliveryCosts.innerHTML = '<p> Lieferkosten: 5 €</p>';
+  total.innerHTML = `Gesamtpreis: ${sum + deliveryCost} €`;
 
-  let total = document.getElementById('total-sum');
  
-  total.innerHTML = "Gesamtsumme: " + (sum + 5) + " €";
+
 
 }
 
 function on() {
   document.getElementById("overlay").style.display = "block";
+  document.getElementById("basket_wrapper").style.display = "block";
+  
 }
 
 function off() {
+  document.getElementById("basket_wrapper").style.display = "none";
   document.getElementById("overlay").style.display = "none";
+  
+ 
+ 
 }
+
